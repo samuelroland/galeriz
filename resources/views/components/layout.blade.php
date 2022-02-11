412cd227bf04eae2c33ec65cee7105557218df0a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-02-11 13:34:59 +0100

    alpinejs: install alpinejs in the project and gitignore public css and js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>PreTPI</title>
     @livewireStyles
 </head>
