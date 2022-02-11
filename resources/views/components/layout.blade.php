54b6c43a6f8c3232c9036c1575a782cf84763cde
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-02-11 11:14:18 +0100

    app: add Livewire 2.10 in the project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PreTPI</title>
-
+    @livewireStyles
 </head>
 
 <body>
     {{ $slot }}
+
+    @livewireScripts
 </body>
 
 </html>
