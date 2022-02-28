3547f993d73cea2d7896180b48c862f1f031dbec
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-02-28 10:40:17 +0100

    Seeder: add 200 images generation with image factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Image;
 use App\Models\Gallery;
 use Illuminate\Database\Seeder;
 
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         $users = User::factory(10)->create();
 
         $galleries = Gallery::factory(30)->create(fn () => ['user_id' => $users->random()->id]);    //Create 30 galleries made by a random user
+
+        $images = Image::factory(200)->create(fn () => ['gallery_id' => $galleries->random()->id]);
     }
 }
