63db7ffb3eacab69fb4c826cba28cb5471bf7620
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-02-28 11:15:13 +0100

    Gallery: update factory and seeder to have a cover image on the cover_id field

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     use HasFactory;
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
