144bec91404ae594190c6408b74462a929957ba3
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-11 15:57:09 +0100

    gallery: design header of the page

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -5,21 +5,22 @@
     <hr>
 
     <div>
-        <h2>{{ $gallery->title }}</h2>
-        <p>{{ $gallery->description }}</p>
-        <p>{{ $gallery->author->name }}</p>
+        <h2 class="text-2xl">{{ $gallery->title }}
+            - by <span class="italic text-violet-800">
+                <a href="{{ '' }}">{{ $gallery->author->name }}</a>
+            </span>
+        </h2>
+        <p class="italic text-sm">{{ $gallery->description }}</p>
+
 
         <div class="flex flex-wrap mt-4 ">
             @foreach ($gallery->images as $image)
-                <a href="{{ route('panorama', ['id']) }}">
-                    <div class="w-44 hover:bg-blue-100 overflow-hidden border border-gray-400 rounded-md m-1 p-1 pb-0">
-                        <img class="w-44 h-44 block rounded-sm" src="/{{ $image->path ?? 'default-cover.png' }}"
-                            alt="">
-                        <div class="whitespace-nowrap overflow-hidden text-ellipsis">
-                            {{ $image->title }}
-                        </div>
+                <div class="w-44 hover:bg-blue-100 overflow-hidden border border-gray-400 rounded-md m-1 p-1 pb-0">
+                    <img class="w-44 h-44 block rounded-sm" src="/{{ $image->path ?? 'default-cover.png' }}" alt="">
+                    <div class="whitespace-nowrap overflow-hidden text-ellipsis">
+                        {{ $image->title }}
                     </div>
-                </a>
+                </div>
             @endforeach
         </div>
     </div>
