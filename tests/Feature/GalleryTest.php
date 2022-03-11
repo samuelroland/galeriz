ba11ecacb2aebdadd168ee9ebe4cac1aba1425b6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-11 08:11:22 +0100

    GalleryTest: fix route name with route() and add 2 tests

--- tests/Feature/GalleryTest.php
+++ tests/Feature/GalleryTest.php
@@ -9,9 +9,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class GalleryTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected $seed = true;
+
     public function test_galleries_page_exists()
     {
-        $response = $this->get('/galleries');
+        $response = $this->get(route('panorama'));
 
         $response->assertStatus(200);
     }
@@ -20,7 +24,7 @@ class GalleryTest extends TestCase
     {
         $galleries = Gallery::all();
 
-        $visitor = $this->get('/galleries');
+        $visitor = $this->get(route('panorama'));
 
         $visitor->assertSeeText($galleries->pluck('title')->toArray());
         $visitor->assertDontSeeText($galleries->pluck('description')->toArray());
@@ -29,7 +33,7 @@ class GalleryTest extends TestCase
     public function test_all_cover_images_are_displayed()
     {
         $galleries = Gallery::all();
-        $visitor = $this->get('/galleries');
+        $visitor = $this->get(route('panorama'));
 
         $galleries->where('cover_id', '!=', null)->each(
             function ($gallery) use ($visitor) {
@@ -41,7 +45,17 @@ class GalleryTest extends TestCase
     // public function test_galleries_have_a_default_cover()
     // {
     //     $galleries = Gallery::all();
-    //     $visitor = $this->get('/galleries');
+    //     $visitor = $this->get(route('panorama'));
     //     $this->assertEquals($galleries->where('cover_id', null)->count(), substr_count("gallery-cover.//png", $visitor->getContent()));
     // }
+
+    public function test_my_galleries_page_exists()
+    {
+        $this->get(route('my'))->assertStatus(200);
+    }
+
+    public function test_gallery_details_page_exists()
+    {
+        $this->get(route('gallery', ['id' => 1]))->assertStatus(200);
+    }
 }
