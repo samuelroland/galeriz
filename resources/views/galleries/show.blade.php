867fdb05c1febb44dfdc39c7f9ec7121095ae475
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-11 13:47:25 +0100

    gallery: start of designing gallery details page

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -1,19 +1,27 @@
 <x-app-layout>
     <div class="my-3">
         <h1 class="text-3xl">Gallery details</h1>
-        <p>TODO.</p>
     </div>
     <hr>
-    <div class="flex flex-wrap mt-4 ">
-        <a href="{{ route('panorama', ['id']) }}">
-            <div class="w-44 hover:bg-blue-100 overflow-hidden border border-gray-400 rounded-md m-1 p-1 pb-0">
-                <img class="w-44 h-44 block rounded-sm"
-                    src="{{ $gallery->cover != null ? $gallery->cover->path : 'default-cover.png' }}" alt="">
-                <div class="whitespace-nowrap overflow-hidden text-ellipsis">
-                    {{ $gallery->title }}
-                </div>
-            </div>
-        </a>
+
+    <div>
+        <h2>{{ $gallery->title }}</h2>
+        <p>{{ $gallery->description }}</p>
+        <p>{{ $gallery->author->name }}</p>
+
+        <div class="flex flex-wrap mt-4 ">
+            @foreach ($gallery->images as $image)
+                <a href="{{ route('panorama', ['id']) }}">
+                    <div class="w-44 hover:bg-blue-100 overflow-hidden border border-gray-400 rounded-md m-1 p-1 pb-0">
+                        <img class="w-44 h-44 block rounded-sm" src="/{{ $image->path ?? 'default-cover.png' }}"
+                            alt="">
+                        <div class="whitespace-nowrap overflow-hidden text-ellipsis">
+                            {{ $image->title }}
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </div>
 
 </x-app-layout>
