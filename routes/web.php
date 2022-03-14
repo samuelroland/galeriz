2996b33f0306d48ea61e77b9038dd288b703ae7c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-14 10:52:47 +0100

    web:  remove useless routes, add middleware to gallery creation routes

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
+use App\Http\Controllers\GalleryController;
 use App\Models\Gallery;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
 Route::get('/', function () {
     $galleries = Gallery::all();
@@ -33,9 +20,11 @@ Route::get('/followed', function () {
     return view('galleries.index', ['galleries' => $galleries]);
 })->name("followedGalleries");
 
-Route::get('/galleries/new', function () {
+Route::middleware(['auth:sanctum'])->get('/galleries/new', function () {
     return view('galleries.create');
 })->name("galleries.new");
+
+Route::middleware(['auth:sanctum'])->post('/galleries/new', [GalleryController::class, 'create'])->name("galleries.new");
 
 Route::get('/galleries/{gallery}', function (Gallery $gallery) {
     return view('galleries.show', ['gallery' => $gallery]);
