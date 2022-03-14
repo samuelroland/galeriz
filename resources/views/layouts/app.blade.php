f97354d2763ae53e700824372bd3783a9024e018
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-14 14:49:02 +0100

    app: test indent file with other extension

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,11 +28,11 @@
 
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
         @endif
 
         <!-- Page Content -->
