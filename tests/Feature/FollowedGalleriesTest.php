dba5e9f266753b4ec36c0c84a9a660d4b7f40366
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-21 09:16:00 +0100

    FollowedGalleriesTest: write 2 missing tests

--- tests/Feature/FollowedGalleriesTest.php
+++ tests/Feature/FollowedGalleriesTest.php
@@ -22,4 +22,19 @@ class FollowedGalleriesTest extends TestCase
 
         $this->actingAs($author)->get(route('followedGalleries'))->assertStatus(200);;
     }
+
+    public function test_followed_galleries_page_is_guarded()
+    {
+        $this->get(route('followedGalleries'))->assertRedirect(route('login'));
+    }
+
+    public function test_it_contains_title_and_description()
+    {
+        $author = User::first();
+
+        $response = $this->actingAs($author)->get(route('followedGalleries'));
+
+        $response->assertSee("Followed galleries");
+        $response->assertSee("Here are all the galleries you follow on Galeriz.");
+    }
 }
