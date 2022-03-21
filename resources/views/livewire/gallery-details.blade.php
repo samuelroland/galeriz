3571d69318fbf21212cacda74b3533c037f5d1c4
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-21 14:27:15 +0100

    GalleryDetails: create Livewire component GalleryDetails

---
+++ resources/views/livewire/gallery-details.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h2 class="text-2xl">{{ $gallery->title }}
+        - by <span class="italic text-violet-800">
+            <a href="{{ '' }}">
+            </a>
+        </span>
+    </h2>
+    <p class="italic text-sm">{{ $gallery->description }}</p>
+</div>
