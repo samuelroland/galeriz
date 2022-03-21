531e67a1ac2ec755caf22d59efec64d8863a5ac6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-21 20:43:21 +0100

    field: fix field $attributes usage

--- resources/views/components/field.blade.php
+++ resources/views/components/field.blade.php
@@ -1,10 +1,10 @@
 @props(['label', 'name' => '', 'type' => 'text'])
-<div {{ $attributes->merge(['class' => 'mt-3']) }}>
+<div {{ $attributes->whereDoesntStartWith('wire:model')->merge(['class' => 'mt-3']) }}>
     <label class="mb-1 block">{{ $label }}</label>
     @if ($type != 'textarea')
-    <input type="{{ $type }}" value="{{ old($name) }}" class="rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
+    <input {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" value="{{ old($name) }}" class="rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
     @else
-    <textarea type="{{ $type }}" class="rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
+    <textarea {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" class="rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
     @endif
 
     @error($name)
