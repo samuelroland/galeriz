05cb3039fa53286abcfee875e3e074f891a7e457
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-21 13:56:50 +0100

    seeder: make sure first user has galleries and fix comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,20 +11,17 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
+        //Create 3 users + 1 fixed user "Sam"
         $users = User::factory(3)->create();
         $users->push(User::factory()->create(['name' => "Sam", 'email' => 'sam@sam.com', 'password' => bcrypt('password')]));   //add special fixed user for testing
 
-        //Create 30 galleries linked to an existing user
-        $galleries = Gallery::factory(7)->create(fn () => ['user_id' => $users->random()->id]);    //Create 30 galleries made by a random user
+        //Create 7 galleries linked to an existing user
+        $galleries = Gallery::factory(7)->create(fn () => ['user_id' => $users->random()->id]);    //Create 7 galleries made by a random user
+        $galleries = Gallery::factory(7)->create(fn () => ['user_id' => $users->first()]);    //Make sure the first user has at least 2 galleries (for testing)
 
-        //Create 200 images linked to an existing category
+        //Create 15 images linked to an existing category
         $images = Image::factory(15)->create(fn () => ['gallery_id' => $galleries->random()->id]);
 
         //Add cover images for a part of the galleries (4/5) with a random image contained in the gallery
