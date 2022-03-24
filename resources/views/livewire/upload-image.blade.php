3cf675dc7676d0bb63d809b56d0fb38ee8c44133
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-24 17:01:18 +0100

    UploadImage: display uploading and uploaded state

--- resources/views/livewire/upload-image.blade.php
+++ resources/views/livewire/upload-image.blade.php
@@ -1,5 +1,5 @@
-<div class="overflow-hidden border border-gray-700 rounded-md p-2 max-w-sm">
-    <h3>Upload a new image</h3>
+<div class="overflow-hidden border border-gray-700 rounded-md p-2 max-w-sm" x-data="{uploaded: false}" x-on:livewire-upload-finish="uploaded = true">
+    <h3 class="text-lg">Upload a new image</h3>
     <x-field label="Title" wire:model.defer="title"></x-field>
     @error('title')
     <span class="text-red-500 italic">{{ $message }}</span>
@@ -9,9 +9,16 @@
     <span class="text-red-500 italic">{{ $message }}</span>
     @enderror
 
-    @if(session()->has('uploadImageMessage'))
-    <span class="text-info">{{ session('uploadImageMessage') }}</span>
-    @endif
 
-    <div class=" flex justify-end"><button wire:click='save' class="btn">Save</button></div>
+
+    <div class="mt-3 flex flex-wrap">
+        <div class="flex-1">
+            @if(session()->has('uploadImageMessage'))
+            <span class="text-message">{{ session('uploadImageMessage') }}</span>
+            @endif
+            <div class="text-message flex-1" wire:loading wire:target='image'>Uploading...</div>
+            <div x-show="uploaded" class="text-message flex-1">The upload is done.</div>
+        </div>
+        <button wire:click='save' @click="uploaded = false" class="btn">Save</button>
+    </div>
 </div>
