396bb209378e9f310b323fff5b71f93d79305a85
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-24 16:30:44 +0100

    ImageGrid: use image grid in show and update views

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -15,18 +15,21 @@
         </h2>
         <p class="italic text-sm">{{ $gallery->description }}</p>
 
-        <div class="flex flex-wrap mt-4 ">
+        {{-- <div class="flex flex-wrap mt-4 ">
             @forelse ($gallery->images as $image)
             <div class="overflow-hidden border border-gray-700 rounded-md m-1 pb-0">
                 <img class="w-full h-72 object-cover block " src="/{{ $image->safePath ?? 'default-cover.png' }}" alt="">
-                <div class="whitespace-nowrap overflow-hidden text-ellipsis max-w-xs mx-2">
-                    {{ $image->title }}
-                </div>
-            </div>
-            @empty
-            <span class="text-message">There is no image in this gallery...</span>
-            @endforelse
+        <div class="whitespace-nowrap overflow-hidden text-ellipsis max-w-xs mx-2">
+            {{ $image->title }}
         </div>
+    </div>
+    @empty
+    <span class="text-message">There is no image in this gallery...</span>
+    @endforelse
+    </div> --}}
+
+    @livewire('image-grid', ['gallery' => $gallery, 'edit' => false])
+
     </div>
 
 </x-app-layout>
--- resources/views/galleries/update.blade.php
+++ resources/views/galleries/update.blade.php
@@ -8,20 +8,8 @@
     <div>
         @livewire('gallery-details', ['gallery' => $gallery])
 
-        <div class="flex flex-wrap mt-4 ">
-            @forelse ($gallery->images as $image)
-            <div class="overflow-hidden border border-gray-700 rounded-md m-1 pb-0">
-                <img class="w-full h-72 object-cover block " src="/{{ $image->safePath ?? 'default-cover.png' }}" alt="">
-                <div class="whitespace-nowrap overflow-hidden text-ellipsis max-w-xs mx-2">
-                    {{ $image->title }}
-                </div>
-            </div>
-            @empty
-            <span class="text-message">There is no image in this gallery...</span>
-            @endforelse
-        </div>
+        @livewire('image-grid', ['gallery' => $gallery, 'edit' => true])
 
-        @livewire('upload-image', ['gallery' => $gallery])
     </div>
 
 </x-app-layout>
