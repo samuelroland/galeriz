13820b1eb3db051d555fa5ec81ecdaf7c38d49b3
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-03-28 10:29:36 +0200

    field: remove prop value and add prop cssOnField

--- resources/views/components/field.blade.php
+++ resources/views/components/field.blade.php
@@ -1,14 +1,17 @@
-@props(['label', 'name', 'type' => 'text', 'value' => ''])
+@props(['label' => '', 'name', 'type' => 'text', 'cssOnField' => ''])
 <div {{ $attributes->whereDoesntStartWith('wire:model')->merge(['class' => 'mt-3']) }}>
+    @if($label != '')
     <label class="mb-1 block">{{ $label }}</label>
+    @endif
+
     @if ($type != 'textarea')
-    <input {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" value="{{ old($name) ?? $value }}" class="rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
+    <input {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
     @else
-    <textarea {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" class="rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) ?? $value }}</textarea>
+    <textarea {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
     @endif
 
     @error($name)
-    <div class="text-red-500 italic">
+    <div class="text-red-500 italic text-sm">
         {{ $message }}
     </div>
     @enderror
